9729d0f04cc045108554077150b1cdb77c9445a4
Paulo19961944 <paulo24041996@gmail.com>
2024-06-07 11:36:13 -0300

    feat: Add API URL

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,8 +1,20 @@
-const weatherBtn = document.getElementById('submitBtn')
-const cityName = document.getElementById('city-name')
+const weatherBtn = document.getElementById('submitBtn');
 
-const weatherPage = weatherBtn.addEventListener('click', (event) => {
-    event.preventDefault();
-    localStorage.setItem('city', cityName.value)
+weatherBtn.addEventListener('click', (event) => {
+  event.preventDefault();
+
+  const cityName = document.getElementById('city-name').value;
+  localStorage.setItem('city', cityName);
+
+  if (cityName === '') {
+    showAlert('Você precisa digitar uma cidade...');
+  } else {
     window.location.href = 'result.html';
+  }
 });
+
+const showAlert = (msg) => {
+  const alertElement = document.getElementById('alert');
+  alertElement.style.display = 'block';
+  alertElement.innerText = msg;
+};
--- src/scripts/weather.js
+++ src/scripts/weather.js
@@ -1,9 +1,45 @@
+// Declaração de Variáveis
 const homeBtn = document.getElementById('btnReturn');
-const showCityName = localStorage.getItem('city');
 const titleCity = document.querySelector('.main-title');
-titleCity.innerText = `${showCityName} - SP`;
+const tempValue = document.getElementById('temp-value')
+const weatherNow = document.getElementById('weather-now')
+const tempMax = document.getElementById('temp-max-value')
+const tempMin = document.getElementById('temp-min-value')
+const moistureValue = document.getElementById('moisture-value')
+const windSpeed = document.getElementById('wind-speed')
 
-// Add event listener to homeBtn
+// Recuperação de Dados Locais
+const cityNameValue = localStorage.getItem('city')
+
+// Chamada da API
+const apiKey = 'deed26ae8dc4a03f86f95d19219b5a98'
+const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${encodeURI(cityNameValue)}&appid=${apiKey}&units=metric&lang=pt_br`
+const results = fetch(apiUrl)
+const getResults = localStorage.getItem('results', results)
+const setResults = localStorage.setItem(getResults)
+const json = setResults.json
+
+
+// Codigo se Bem Sucedido
+if(json.cod === 200){
+  showInfo({
+    city: json.name,
+    country: json.sys.country,
+    tempNow: json.main.temp,
+    tempMax: json.main.temp_max,
+    tempMin: json.main.temp_min,
+    humidity:temp.main.humidity,
+    windSpeed: temp.wind.speed,
+    description: json.weather[0].description,
+  })
+}
+
+// Mostra as Informações
+const showInfo = (json) => {
+  titleCity.innerText = `${json.city} - ${json.country}`;
+}
+
+// Adiciona Evento de Clique ao Botão
 homeBtn.addEventListener('click', () => {
   window.location.href = 'index.html';
 });
